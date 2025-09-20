69e32a21c0532bc1f57e19674d89c4bafda9ba43
yalta <yalta@fake.sus>
2025-09-20 11:20:08 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using System;
+using System.IO;
+
+class Program
+{
+    private static ITelegramBotClient _botClient;
+
+    private static ReceiverOptions _receiverOptions;
+    public static void Main()
+
+    // static async Task Main()
+    {
+        string filePath = "token.txt";
+        // foreach (string line in File.ReadLines(filePath))
+        // {
+        //     Console.WriteLine(line);
+        // }
+        Console.WriteLine(File.ReadLines(filePath).First());
+        // _botClient = new TelegramBotClient("<token>");
+        // _receiverOptions = new ReceiverOptions
+        // {
+        //     AllowedUpdates = new[]{
+        //         UpdateType.Message,
+        //     },
+        // }
+    }
+}
